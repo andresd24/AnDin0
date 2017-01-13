f6c2b715136a1684ee71c2402b53c543207bc79d
andresd24 <andresdev@gmail.com>
2017-01-13 07:48:01 -0500

    first commit

---
+++ src/app/services/reddit.service.ts
@@ -0,0 +1,33 @@
+/**
+ * Created by AnDin0 on 12/01/17.
+ */
+
+import  {Injectable} from  '@angular/core';
+import {Http} from  '@angular/http';
+import 'rxjs/Rx';
+
+@Injectable()
+export class RedditService {
+    http: any;
+    baseUrl: String;
+
+
+    constructor(http:Http)
+    {
+        this.http = http;
+        this.baseUrl = 'https://www.reddit.com/r';
+
+    }
+
+
+    getPosts(category, limit)
+    {
+
+        return this.http.get(this.baseUrl + '/' + category + '/top.json?limit=' + limit).
+          map(res => res.json());
+
+
+    }
+}
+
+
