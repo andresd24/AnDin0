b3961e2e8c431b5271c5ad912eaaac516db9e63d
andresd24 <andresdev@gmail.com>
2017-01-15 21:25:41 -0500

    esto funcion sin XDR

--- src/app/services/reddit.service.ts
+++ src/app/services/reddit.service.ts
@@ -4,30 +4,72 @@
 
 import  {Injectable} from  '@angular/core';
 import {Http} from  '@angular/http';
-import 'rxjs/Rx';
+import 'rxjs/Rx'
+import './xhr-xdr-adapter.js';
+import {Response} from '@angular/http';
 
+//import {@CrossOrigin} from './xhr-xdr-adapter.js';
+
+
+declare var XDomainRequest: { new (); create(); };
+
+
+//@CrossOrigin()
 @Injectable()
 export class RedditService {
     http: any;
     baseUrl: String;
+    method: String;
+    responseText: String;
 
 
     constructor(http:Http)
     {
         this.http = http;
-        this.baseUrl = 'https://www.reddit.com/r';
+        //this.baseUrl = 'https://www.reddit.com/r';
+        this.baseUrl = 'http://localhost:3000/api/v1/heroes/';
 
     }
 
 
-    getPosts(category, limit)
-    {
+  getPosts(category, limit)
+  {
 
-        return this.http.get(this.baseUrl + '/' + category + '/top.json?limit=' + limit).
-          map(res => res.json());
+   // return this.http.get(this.baseUrl + '/' + limit).
+   //c© map(res => res.json());
 
+  }
 
+  createCORSRequest(method:string, url:string){
+    var xhr = new XMLHttpRequest();
+
+    if ("withCredentials" in  xhr){
+      xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined"){
+      xhr = new XDomainRequest();
+      xhr.open(method, url);
+
+      if (url != null) {
+        xhr.setRequestHeader("Access-Control-Allow-Origin", url);
+      } else {
+        xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+      }
+    } else {
+      xhr = null;
     }
+    return xhr.send();
+  }
+
+/*
+  request = this.createCORSRequest(method, "http://www.nczonline.net/");
+  if (request){
+    request.onload = function(){
+      //do something with request.responseText
+    };
+    request.send();
+  }*/
+
+
 }
 
 
--- src/pages/reddits/reddits.ts
+++ src/pages/reddits/reddits.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { RedditService} from '../../app/services/reddit.service'
-
+import  "rxjs/add/operator/map";
+import {Response} from '@angular/http';
 
 @Component({
   selector: 'reddits',
@@ -9,21 +10,33 @@ import { RedditService} from '../../app/services/reddit.service'
 })
 export class RedditsPage {
   items: any;
+  responseText: String;
 
   constructor(public navCtrl: NavController, private redditService:RedditService) {
 
   }
 
 
-  ngOnInit() {
-      this.getPosts('sports',5);
+  ngOnInit()
+  {
+       this.redditService.createCORSRequest('GET', 'http://localhost:3000/api/v1/heroes');
   }
 
-  getPosts(category, limit) {
+    /*  getHeroes(category, limit) {
+     this.redditService.createCORSRequest('GET','http://localhost:3000/api/v1/heroes').subscribe(response => {
+     this.items = response.data.children });
 
-      this.redditService.getPosts(category, limit).subscribe(response => {
-         this.items = response.data.children });
+     }
+     */
 
-  }
+    /*  getHeroes(cat, cart) {
+
+     this.redditService.createCORSRequest('GET', 'http://localhost:3000/api/v1/heroes').subscribe(response => {
+     this.items = response.data.children });
+     }
+
+
+     */
+
 
 }
